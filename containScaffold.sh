b1cf0c18bfdf8341322750849a0bc823a207a6c2
jdifuria <jaddifuria@gmail.com>
2015-12-10 01:42:35 -0800

    Added script to wrap a scaffolds views in container divs

---
+++ containScaffold.sh
@@ -0,0 +1,12 @@
+#!/bin/bash/
+sed -i '1i\<div class="container">' edit.html.erb
+sed -i '1i\<div class="container">' index.html.erb
+sed -i '1i\<div class="container">' new.html.erb
+sed -i '1i\<div class="container">' _form.html.erb
+sed -i '1i\<div class="container">' show.html.erb
+sed -i "\$a</div>" edit.html.erb
+sed -i "\$a</div>" index.html.erb
+sed -i "\$a</div>" new.html.erb
+sed -i "\$a</div>" _form.html.erb
+sed -i "\$a</div>" show.html.erb
+echo "Your views for $(basename "$PWD") now are wrapped in container divs"
