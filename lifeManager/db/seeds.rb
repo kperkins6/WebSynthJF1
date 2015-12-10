d8007677de4c9a0a339eda548ce3fbb3d4240fa5
jdifuria <jaddifuria@gmail.com>
2015-12-10 01:02:31 -0800

    Update seeds file with articles and tags

--- lifeManager/db/seeds.rb
+++ lifeManager/db/seeds.rb
@@ -5,6 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# uses this dictionary to generate random tags
+load "#{Rails.root}/db/words.rb"
+
+def gen_tag
+  str = String.new
+  len = rand(4) + 1
+  for i in 1..len
+    str += $words.sample + " "
+  end
+  return str
+end
+
 users = User.create(
   [
     { email: 'a@a.com', name: "Mrs. Aardvark", password: "a", password_confirmation: "a"},
@@ -35,3 +47,49 @@ users = User.create(
     { email: 'z@z.com', name: "Mrs. Zebra", password: "z", password_confirmation: "z"},
   ]
 )
+
+
+articles = Article.create(
+  [
+  name:string url:string user_id:integer
+
+    { name: "Mocha Tutorial", url: "https://semaphoreci.com/community/tutorials/getting-started-with-node-js-and-mocha", user_id: 1 },
+    { name: "DJTT - Ableton Live", url: "http://djtechtools.com/2012/04/18/how-to-start-djing-in-ableton-part-1/", user_id: 1 },
+    { name: "Using Ableton for DJing", url: "http://www.soundonsound.com/sos/jul10/articles/livetech_0710.htm", user_id: 3 },
+    { name: "Ableton", url: "https://www.ableton.com/", user_id: 1 },
+    { name: "What is sound?", url: "http://www.soundproofingcompany.com/soundproofing101/what-is-sound/", user_id: 1 },
+    { name: "Using Sound", url: "https://helpx.adobe.com/audition/using/sound.html", user_id: 2 },
+    { name: "DJTT - Gift Guide", url: "http://djtechtools.com/2015/12/09/holiday-gift-guide-for-djs-producers-2015-edition/", user_id: 3 },
+    { name: "DJTT - Pan Pot", url: "http://djtechtools.com/2015/12/07/pan-pot-interview-recording-a-kalimba-for/", user_id: 1 },
+    { name: "DJTT - Midi-mapped filter", url: "http://djtechtools.com/2015/12/03/use-midi-mapped-ableton-filter-mix-tracks/", user_id: 2 },
+    { name: "DJTT - Dolby Atmos", url: "http://djtechtools.com/2015/11/24/dolby-atmos-coming-to-a-club-near-you/", user_id: 3 },
+    { name: "DJTT - TB-303", url: "http://djtechtools.com/2015/12/02/history-tb-303-rolands-accidental-legend/", user_id: 1 },
+    { name: "DJTT - Pioneer DJM", url: "http://djtechtools.com/2015/11/23/pioneer-djm-s9-review-new-hybrid-battle-mixer/", user_id: 2 },
+    { name: "DJTT - Warmup DJs", url: "http://djtechtools.com/2015/11/20/warmup-djs-play-to-the-crowd/", user_id: 3 },
+
+  ]
+)
+
+#this code was sourced from Tyson Henry
+articles.each do |article|
+
+  # create access objects for current article
+  if article.private
+    user_indexes = Array.new
+    size = 3 + rand(6)
+    for i in 0..size
+      while (user_indexes[i] = (rand 25) + 1) == article.user_id
+      end
+    end
+    user_indexes.uniq!
+    user_indexes.each do |user_id|
+      articleUser.create(user_id: user_id, article_id: article.id)
+    end
+  end
+  
+  # create tags
+  for i in 0..rand(4)
+    random_tag_string = gen_tag
+    Tag.create(article_id: article.id, str: random_tag_string)
+  end
+end
