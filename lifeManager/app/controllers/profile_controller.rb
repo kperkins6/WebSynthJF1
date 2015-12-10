0c408b21c36e2946561fe642b40e6b3ad85f15b9
jdifuria <jaddifuria@gmail.com>
2015-12-10 01:51:31 -0800

    Update profile controller with full data access

--- lifeManager/app/controllers/profile_controller.rb
+++ lifeManager/app/controllers/profile_controller.rb
@@ -1,4 +1,11 @@
 class ProfileController < ApplicationController
   def index
+    @schedules = Schedule.all
+    @checklists = Checklist.all
+    @users = User.all
+    @articles = Article.all
+    @tags = Tag.all
+    @tasks = Tasks.all
+    @events = Event.all
   end
 end
