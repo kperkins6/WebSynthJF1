d399ce4cf7b70be7927827faa7245108c88e4623
jdifuria <jaddifuria@gmail.com>
2015-12-10 17:52:24 -0800

    Update task controller with json returns

--- lifeManager/app/controllers/tasks_controller.rb
+++ lifeManager/app/controllers/tasks_controller.rb
@@ -20,6 +20,10 @@ class TasksController < ApplicationController
   # GET /tasks/1
   # GET /tasks/1.json
   def show
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @task}
+    end
   end
 
   # GET /tasks/new
@@ -38,7 +42,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
@@ -52,7 +56,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to root_url, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
@@ -66,7 +70,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
