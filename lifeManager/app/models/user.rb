55c3fcbb43b70ab3956e775ed7563715405272f2
jdifuria <jaddifuria@gmail.com>
2015-12-12 06:59:33 -0800

    FIX for devise username field addition

--- lifeManager/app/models/user.rb
+++ lifeManager/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  attr_accessor :login
   validates :email, presence: true
   validates :name, presence: true
   validates :password, presence: true
