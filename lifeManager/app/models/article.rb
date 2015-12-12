08d24a1f58b1c4008cc49ba9026e0482afcb7334
jdifuria <jaddifuria@gmail.com>
2015-12-12 09:08:01 -0800

    Update article model with url validation.  Not working

--- lifeManager/app/models/article.rb
+++ lifeManager/app/models/article.rb
@@ -1,6 +1,31 @@
 class Article < ActiveRecord::Base
+  require "net/http"
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :user_id, presence: true
+  #validate :is_real
   belongs_to :user
-  has_many :tags, dependent: :destroy
-  
+  has_many :tags, dependent: :destroy  
+ 
   accepts_nested_attributes_for :tags
+   
+  private 
+  def prePend
+    unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
+      self.url = "https://#{self.url}"
+    end 
+  end 
+
+  def is_real
+    prePend
+    uri = URI(self.url)
+    req = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP.get_response uri
+
+    if req.code.to_i != 200
+      errors.add(self.url, "Site did not return a valid response code")
+    end
+
+  end  
+
 end
